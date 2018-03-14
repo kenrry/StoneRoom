512574bcb0f3e945e17af34f299cbb0fe1c36ae2
kenrry <wangyifan495@pingan.com.cn>
2018-03-14 13:36:56 +0800

    自动化脚本

---
+++ StoneRoom/xcodebuild.sh
@@ -0,0 +1,32 @@
+#项目的SCHEMENAME
+SCHEMENAME=StoneRoom
+#项目的分支
+BRANCHNAME=master
+ADHOCExportOptionsPlist=./ADHOCExportOptionsPlist.plist
+AppStoreExportOptionsPlist=./AppStoreExportOptionsPlist.plist
+EnterpriseExportOptionsPlist=./EnterpriseExportOptionsPlist.plist
+
+ADHOCExportOptionsPlist=${ADHOCExportOptionsPlist}
+AppStoreExportOptionsPlist=${AppStoreExportOptionsPlist}
+EnterpriseExportOptionsPlist=${EnterpriseExportOptionsPlist}
+
+DATE=`date +%Y%m%d_%H%M`
+SOURCEPATH="/Users/wangyifan495/Desktop/杂记/StoneRoom/StoneRoom"
+ARCHIVEPATH=$SOURCEPATH/archive/$SCHEMENAME.xcarchive
+IPAPATH=$SOURCEPATH/AutoBuildIPA/$BRANCHNAME/$DATE
+
+echo "==================== 准备打包 ===================="
+xcodebuild archive -workspace $SOURCEPATH/StoneRoom.xcworkspace \
+-scheme $SCHEMENAME \
+-configuration Release \
+-archivePath ${ARCHIVEPATH} \
+CODE_SIGN_IDENTITY="iPhone Distribution: wang yifan (HU34BCTTPG)" \
+PRODUCT_BUNDLE_IDENTIFIER="com.StoneRoom.developfan"
+
+echo "==================== 准备导出 ===================="
+xcodebuild -exportArchive \
+-archivePath ${ARCHIVEPATH} \
+-exportOptionsPlist $ADHOCExportOptionsPlist \
+-exportPath ${IPAPATH} \
+
+
